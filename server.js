c0325e3f331216e56d769d92cb82dbda006db1ea
Sangram Sampat Nangare <itsmesangram5@gmail.com>
2024-09-21 18:01:47 +0530

    Minor Changes in Server.js

--- server.js
+++ server.js
@@ -24,12 +24,13 @@ app.use("/api/v1/auth", require("./routes/userRoutes"));
 app.use("/api/v1/post", require("./routes/postRoutes"));
 
 //home
-app.get("/",(eq,res)=>[
+app.get("/", (req, res) => {
   res.status(200).send({
-    "success":true,
-    "msg":"Server is running"
-  ])
-  })
+    success: true,
+    msg: "Server is running"
+  });
+});
+
 
 //PORT
 const PORT = process.env.PORT || 8080;
